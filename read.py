9ac084b16276a66b8c485abe1dbcc93246bf7090
Pin-Chun Huang <pt825123@yahoo.com.tw>
2022-04-21 16:59:54 +0800

    calc average reviews

--- read.py
+++ read.py
@@ -1,10 +1,15 @@
 data = []
 count = 0
+sum_len = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
 		data.append(line)
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
-print(len(data))
-print(data[0])
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+for d in data:
+	sum_len += len(d)
+average = sum_len / len(data)
+print('平均留言長度為', average)
